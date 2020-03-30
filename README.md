ced26c6bcab857c72ef3c6cfcfe826a900314506
Ivan <glushenkov.ig@phystech.edu>
2020-03-30 12:50:26 +0300

    Добавил лабораторную 3 (reddis + http сервер)

--- README.md
+++ README.md
@@ -1,18 +1,46 @@
-
 https://habr.com/ru/post/310460/
 
-docker container stop $(docker container ls -aq)
-
-
-docker-composer up
-
+##Лаба 1: Запустить в докере проект с прошлого семестра
 docker ps
 
 docker images
 
-docker build -p host:container -i -t name .
+docker build .
 
+docker run -p host_port:container_port -it name_of_image 
 
+##Лаба 2: разбить на 3 сервиса и в докеркомпоуз
+копируем всю папку с проектом 3 раза и выпиливаем из каждой два ненужных сервиса
+собираем каждый в докере через Dockerfile, тестим, запускаем в компоузе
+
+docker-composer up
+https://docs.docker.com/compose/gettingstarted/
+
+##Лаба 3: подключить БД
+
+Ищем какой самый простой kv-storage
+https://www.reddit.com/r/docker/comments/c5sgnq/what_is_the_smallest_simplest_keyvalue_store/
+ищем обёртку для него в виде http сервера
+Находим https://github.com/nicolasff/webdis
+
+запускаем в докер компоуз как отдельный сервер
+вот Гайд: https://docs.docker.com/compose/gettingstarted/
+(говорят хорошая статья: https://habr.com/ru/post/310460/)
+
+Пишем обёртки обращений к базе в нашем коде (используем как kV-storage)
+https://redis.io/commands/set
+
+Проверяем что всё работает (порты проброшены и прописаны в docker-compose.yml, можно чекать с браузера)
+
+##Лаба 4: Ansible playbooks
+написать ansible playbook для разворачивания готовых сервисов на два сервака.  
+1) Чекаем презу
+2) Смотрим видосик:
+3) Пункты
+
+Лаба 5: используя liquibase или flyway написать инкрементальные скрипты накатки обновлений на БД , которые ставятся вместе с приложением (т.е. еще надо будет обновить playbook ansible). 
+Лаба 6: Всю историю с докером переделать под kubernetes
+Лаба 7: Будем делать мониторинг и дашборды нашего приложения/сервиса.
 
 
 
--- module_predict/src/main/java/hello/ExchangePrediction.java
+++ module_predict/src/main/java/hello/ExchangePrediction.java
@@ -1,28 +1,82 @@
 package hello;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.LinkedHashMap;
+
 @Service
 public class ExchangePrediction {
     Weather weather;
+    String host_url = "http://db:7379/";
+
     public ExchangePrediction(){
         this.weather = new Weather();
     }
 
     public double predictExchange( double exchange){
-        return (requestWeather() + exchange)/2;
+        try {
+            if(db_contains(exchange) == 1 ){
+                return db_load(exchange);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Double weather = requestWeather();
+        db_store(exchange, weather);
+        return ( + exchange)/2;
     }
 
-    private int requestWeather() {
+    private Double requestWeather() {
         RestTemplate restTemplate = new RestTemplate();
+
         String requestURL
                 = "http://weather:8082/weather";
         ResponseEntity<String> response = restTemplate.getForEntity(requestURL, String.class);
+        Double weather = Double.parseDouble(response.getBody());
 
-        return Integer.parseInt(response.getBody());
+        return weather;
+    }
+
+    private Integer db_contains(double exchange) throws JSONException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String requestURL
+                = host_url + "EXISTS/" + exchange;
+        ResponseEntity<LinkedHashMap> response1 = restTemplate.getForEntity(requestURL, LinkedHashMap.class);
+        // todo the problem
+        // https://github.com/nicolasff/webdis
+        // docker run --rm -d -p 8184:7379 webdis
+
+        // http://127.0.0.1:8083/predict/39
+        // http://127.0.0.1:8184/GET/39
+        System.out.println(response1.getBody()); // {EXISTS=1}
+        ResponseEntity<LinkedHashMap> response = restTemplate.getForEntity(requestURL, LinkedHashMap.class);
+        // System.out.println(String.format("-------------------------" + response.getBody(), 4));
+
+        System.out.println(response.getBody()); // {}
+        return Integer.parseInt(response.getBody().get("EXISTS").toString());
+    }
+
+    private double db_load(double exchange){
+        RestTemplate restTemplate = new RestTemplate();
+
+        String requestURL
+                = host_url + "GET/" + exchange;
+        ResponseEntity<LinkedHashMap> response = restTemplate.getForEntity(requestURL, LinkedHashMap.class);
+        System.out.println(response.getBody().getClass());
+        return Double.parseDouble(response.getBody().get("GET").toString());
+    }
+
+    private void db_store(double exchange, Double weather){
+        RestTemplate restTemplate = new RestTemplate();
+
+        String requestURL
+                = host_url + "SET/" + exchange + "/" + weather;
+        ResponseEntity<LinkedHashMap> response = restTemplate.getForEntity(requestURL, LinkedHashMap.class);
     }
 }
